c979f28c4aefda3b6810ca8906517b9c5733e0d3
CLINTPS <clintps1010@gmail.com>
2024-10-23 11:12:48 +0530

    refactor: auth service script update

--- auth-service/src/domain/entites/otpEntity.ts
+++ auth-service/src/domain/entites/otpEntity.ts
@@ -1,7 +1,7 @@
 import { Document,ObjectId } from "mongoose";
 
 export interface IOtp extends Document{
-    _id?:ObjectId,
+    _id:ObjectId,
     email?:string,
     otp?:string,
     createdOn?:Date
